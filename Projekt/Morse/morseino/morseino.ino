2feb00edea350a4fc0b6f968a3a04d0a16f16fe1
Johan Kämpe <kampe.johan@gmail.com>
2017-02-28 11:06:46 +0100

    Built functions

--- Projekt/Morse/morseino/morseino.ino
+++ Projekt/Morse/morseino/morseino.ino
@@ -1,4 +1,11 @@
+/*   Programmering av inbyggda system
+ *  "Morse"
+ *   Send letters as morse code via LED/IR-LED
+*/
+
 #define PIN_IR 8
+
+void sendMorse(char text[]);
 
 void setup(){
     pinMode(PIN_IR, OUTPUT);
@@ -6,37 +13,52 @@ void setup(){
 }
 
 void loop(){
-    morseS();
-    morseO();
-    morseS();
     digitalWrite(PIN_IR, LOW);
-    delay(1000);
+    sendMorse("sos");
 }
 
+/*    Send text string as morse code */
+void sendMorse(char text[]){
+    for(int i = 0; i < strlen(text); i++){
+        Serial.print(text[i]);
+        switch(tolower(text[i])){
+            case 's':
+                morseS();
+                break;
+            case 'o':
+                morseO();
+                break;
+            default:
+                break;
+        }
+    }
+    Serial.println("_");
+}
+
+/*    Ouput of letter S in morse code   */
 void morseS(){
     short_c();
-    delay(100);
     short_c();
-    delay(100);
     short_c();
-    delay(100);
+    delay(2000);
 }
 
+/*    Ouput of letter O in morse code   */
 void morseO(){
     long_c();
-    delay(100);
     long_c();
-    delay(100);
     long_c();
-    delay(100);
+    delay(2000);
 }
 
+/*    Send "long"   */
 void long_c(void){
     digitalWrite(PIN_IR, HIGH);
     delay(1000);
     digitalWrite(PIN_IR, LOW);
 }
 
+/*    Send "short"   */
 void short_c(void){
     digitalWrite(PIN_IR, HIGH);
     delay(400);
