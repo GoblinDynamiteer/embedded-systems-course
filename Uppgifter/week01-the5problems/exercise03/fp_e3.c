194407aabf1a154a7681a9c22f5e61c0f210867d
Johan Kämpe <kampe.johan@gmail.com>
2017-03-04 11:18:43 +0100

    The 5 problems: Problem 3 solved
    Printout of higher fibonnaci numbers are incorrect though

---
+++ Uppgifter/week01-the5problems/exercise03/fp_e3.c
@@ -0,0 +1,58 @@
+/*
+The 5 problems
+Problem 3
+
+Write a function that computes the list
+of the first 100 Fibonacci numbers.
+By definition, the first two numbers in
+the Fibonacci sequence are 0 and 1, and
+each subsequent number is the sum of the
+previous two.
+
+As an example, here are the first 10
+Fibonnaci numbers:
+    0, 1, 1, 2, 3, 5, 8, 13, 21, and 34.
+
+Johan Kämpe
+Mjukvaruutvecklare Inbyggda System
+
+Kurs
+Programmering av inbyggda system
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*  To hold higher fibonacci numbers,
+    cant use for all 100 though. */
+typedef unsigned long long int ulli;
+
+ulli fibonacci(int n);
+void fibonacci100();
+
+int main(int argc, char const *argv[]) {
+    fibonacci100();
+    return 0;
+}
+
+/*   Calculate fibonacci number n    */
+ulli fibonacci(int n){
+    if(!n){
+        return 0;
+    }
+    ulli num1 = 0, num2 = 1;
+    while(--n){
+        ulli temp = num2;
+        num2 = num1 + num2;
+        num1 = temp;
+    }
+    return num2;
+}
+
+/*    Print the first 100 fibonacci numbers */
+void fibonacci100(){
+    for(int i = 0; i < 100; i++){
+        /*  Unsigned long long is not big enough for higher numbers. */
+        printf("%03i : %u\n", i+1, fibonacci(i));
+    }
+}
