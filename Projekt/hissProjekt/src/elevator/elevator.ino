e2043a02a13812226494b0b13ed605b0d2135040
Johan Kämpe <kampe.johan@gmail.com>
2017-03-09 17:56:13 +0100

    Added queue system for elevator car

--- Projekt/hissProjekt/src/elevator/elevator.ino
+++ Projekt/hissProjekt/src/elevator/elevator.ino
@@ -43,6 +43,8 @@ enum {pincallButton1, pincallButton2, pincallButton3,
     pinelevLED1, pinelevLED2, pinelevLED3
 };
 
+#define NUM_FLOORS 3
+
 /*    Init buttons   */
 Button callButton1(pincallButton1);
 Button callButton2(pincallButton2);
@@ -51,10 +53,24 @@ Button elevButton1(pinelevButton1);
 Button elevButton2(pinelevButton2);
 Button elevButton3(pinelevButton3);
 
+/*   Elevator car queue  */
+int carQueue[NUM_FLOORS];
+
 /*    Function prototypes   */
+/*    Elevator car function prototypes */
 int elevatorFloorPosition();
 void moveElevatorToFloor(int floor);
 void callElevatorToFloor(int floor);
+
+/*   LED function prototypes  */
+void setIndicatorLED(int floor);
+void setCallButtonLEDOn(int floor);
+void setCallButtonLEDOff(int floor);
+
+/*   Elevator car queue function prototypes  */
+void addToCarQueue(int floor);
+void handleCarQueue(void);
+void removeFromCarQueue(int floor);
 
 /*   Moves elevator car to passed floor    */
 void moveElevatorToFloor(int floor){
@@ -64,20 +80,66 @@ void moveElevatorToFloor(int floor){
         elevatorCurrentFloor = floor;
     }
     delay(1000);
-    /*   Turn of LEDs that indicate elevator car position    */
-    digitalWrite(pinelevLED1, LOW);
-    digitalWrite(pinelevLED2, LOW);
-    digitalWrite(pinelevLED3, LOW);
-    /*   Turn on LED for new elevator car floor positon    */
-    if(floor == 1){
-        digitalWrite(pinelevLED1, HIGH);
+    setIndicatorLED(floor);
+    delay(500);
+    setCallButtonLEDOff(floor);
+}
+
+/*   Add floor to elevator car queue  */
+void addToCarQueue(int floor){
+  for(int i = 0; i < NUM_FLOORS; i++){
+    /*  Don't add floor already in queue   */
+    if((!carQueue[i]) && (carQueue[i] != floor)){
+      carQueue[i] = floor;
+      break;
     }
-    else if(floor == 2){
-        digitalWrite(pinelevLED2, HIGH);
+  }
+}
+
+/*   Remove floor from car queue,
+  elevator has arrived at this floor  */
+void removeFromCarQueue(int floor){
+  for(int i = 0; i < NUM_FLOORS; i++){
+    if(carQueue[i] == floor){
+      carQueue[i] = 0;
     }
-    else{
-        digitalWrite(pinelevLED3, HIGH);
+  }
+}
+
+/*     */
+void handleCarQueue(void){
+  int nextFloor = 0;
+  for(int i = 0; i < NUM_FLOORS; i++){
+    if(!carQueue[i]){
+      nextFloor = carQueue[i];
+      break;
     }
+  }
+  if(nextFloor){
+    moveElevatorToFloor(nextFloor);
+    removeFromCarQueue(nextFloor);
+  }
+}
+
+/*  Turns elevator car indicator LED on   */
+void setIndicatorLED(int floor){
+  /*   Turn off LEDs that indicate elevator car position    */
+  for(int ledPin = pinelevLED1;
+      ledPin < pinelevLED1 + NUM_FLOORS; ledPin++){
+        digitalWrite(ledPin, LOW);
+  }
+  /*   Turn on LED for new elevator car floor positon    */
+      digitalWrite(pinelevLED1 + floor - 1, HIGH);
+}
+
+/*  Turns call button indicator LED on   */
+void setCallButtonLEDOn(int floor){
+      digitalWrite(pincallLED1 + floor - 1, HIGH);
+}
+
+/*  Turns call button indicator LED on   */
+void setCallButtonLEDOff(int floor){
+      digitalWrite(pincallLED1 + floor - 1, LOW);
 }
 
 /*    Returns elevetor car floor position   */
@@ -87,65 +149,59 @@ int elevatorFloorPosition(){
 
 /*    Function that calls elevator to a floor.   */
 void callElevatorToFloor(int floor){
-    /*   Turn of LEDs   */
-    digitalWrite(pincallLED1, LOW);
-    digitalWrite(pincallLED2, LOW);
-    digitalWrite(pincallLED3, LOW);
     /*    Turn on correct callLED   */
-    if(floor == 1){
-        digitalWrite(pincallLED1, HIGH);
-    }
-    else if(floor == 2){
-        digitalWrite(pincallLED2, HIGH);
-    }
-    else{
-        digitalWrite(pincallLED3, HIGH);
-    }
+    setCallButtonLEDOn(floor);
     delay(1000);
     /*   Moves the elevator car to the correct floor and turn off callLEDs   */
     moveElevatorToFloor(floor);
     delay(500);
-    digitalWrite(pincallLED1, LOW);
-    digitalWrite(pincallLED2, LOW);
-    digitalWrite(pincallLED3, LOW);
+    setCallButtonLEDOff(floor);
 }
 
 void setup(){
     /*   Set pinmode for LED pins and start buttons    */
-    pinMode(pincallLED1, OUTPUT);
-    pinMode(pincallLED2, OUTPUT);
-    pinMode(pincallLED3, OUTPUT);
-    pinMode(pinelevLED1, OUTPUT);
-    pinMode(pinelevLED2, OUTPUT);
-    pinMode(pinelevLED3, OUTPUT);
+    for(int i = 0; i < NUM_FLOORS; i++){
+      pinMode(pincallLED1 + i, OUTPUT);
+      pinMode(pinelevLED1 + i, OUTPUT);
+      /*  NULL elevator car queue   */
+      carQueue[i] = 0;
+    }
+    /*   Elevator car start floor, middle of building  */
+    carQueue[0] = NUM_FLOORS / 2 + 0.5;
     callButton1.begin();
     callButton2.begin();
     callButton3.begin();
     elevButton1.begin();
     elevButton2.begin();
     elevButton3.begin();
+    //Serial.begin(9600);
 }
 
 void loop(){
     /*   loop for elevator call buttons    */
     if(callButton1.pressed()){
-        callElevatorToFloor(1);
+      addToCarQueue(1);
+      setCallButtonLEDOn(1);
     }
-    else if(callButton2.pressed()){
-        callElevatorToFloor(2);
+    if(callButton2.pressed()){
+      addToCarQueue(2);
+      setCallButtonLEDOn(2);
     }
-    else if(callButton3.pressed()){
-        callElevatorToFloor(3);
+    if(callButton3.pressed()){
+      addToCarQueue(3);
+      setCallButtonLEDOn(3);
     }
 
     /*   loop for elevator buttons    */
     if(elevButton1.pressed()){
-        moveElevatorToFloor(1);
+      addToCarQueue(1);
     }
-    else if(elevButton2.pressed()){
-        moveElevatorToFloor(2);
+    if(elevButton2.pressed()){
+      addToCarQueue(2);
     }
-    else if(elevButton3.pressed()){
-        moveElevatorToFloor(3);
+    if(elevButton3.pressed()){
+      addToCarQueue(3);
     }
+
+    handleCarQueue();
 }
