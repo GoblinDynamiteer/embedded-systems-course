34f9c9697cb5973d1dc47b920fbbfd97cff68e2c
Johan Kämpe <kampe.johan@gmail.com>
2017-04-11 12:45:03 +0200

    Comments

--- Projekt/keypadAVR/keypad/main.c
+++ Projekt/keypadAVR/keypad/main.c
@@ -1,18 +1,26 @@
+/*	Keypad to AVR
+	Atmega328p
+	12 key keypad
+	3x4 buttons
+	*/
+
+
 #include <avr/io.h>
 
+/*	 Defined in project properties	*/
 //#define F_CPU 1000000ul
 #include <util/delay.h>
 
 void blinkLed(int n);
 int checkPin(int pin);
 
-/*	 Row pins	*/
+/*	 Row pins - for toggling output	*/
 enum {	ROW1_PIN = (0b00000100), 
 		ROW2_PIN = (0b10000000), 
 		ROW3_PIN = (0b01000000)
 		};
 		
-/*	 Column pins	*/
+/*	 Column pins - for checking input	*/
 enum {	COL1_PIN = PD3,
 		COL2_PIN = PD1,
 		COL3_PIN = PD5
@@ -20,25 +28,29 @@ enum {	COL1_PIN = PD3,
 	
 int main(void){
 	DDRC = 0b00100000; //LED Output
-	DDRD = 0b11000100;
+	DDRD = 0b11000100; //Column pins output
 	
-	int columnPin[4] = {0, COL1_PIN, COL2_PIN, COL3_PIN};
-	int rowPin[4] = {0, ROW1_PIN, ROW2_PIN, ROW3_PIN};
+	int columnPin[3] = {COL1_PIN, COL2_PIN, COL3_PIN};
+	int rowPin[3] = {ROW1_PIN, ROW2_PIN, ROW3_PIN};
 	
-	//blinkLed(10);
 	while(1){
 		int num = 1;
-		for(int j = 1; j <= 3; j++){
+		/*	 Set row output	*/
+		for(int j = 0; j < 3; j++){
 			 PORTD = rowPin[j];
 			 _delay_ms(10);
-			for(int i = 1; i <= 3; i++){
-				if(checkPin(columnPin[i]))	{blinkLed(num);	}
+			/*	 Check column input	*/
+			for(int i = 0; i < 3; i++){
+				if(checkPin(columnPin[i])){
+					blinkLed(num);
+				}
 				num++;
 			}
 		}
 	}
 }
 
+/*	 Blink led n times	*/
 void blinkLed(int n){
 	for(int i = 0; i < n; i++){
 		 PORTC = 0b00100000;
@@ -48,6 +60,7 @@ void blinkLed(int n){
 	}
 }
 
+/*	 Check digital input on pin, returns 1 / 0	*/
 int checkPin(int pin){
 	return (PIND & (1 << pin));
 }
